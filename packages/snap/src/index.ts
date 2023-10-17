74f780f144c11fc4703b4cf1108b77bf2380034b
Kami <ekbatanifard@gmail.com>
2023-10-17 21:30:33 +0300

    refactor

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,4 +1,5 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
+
 import { createAddress } from './rpc/createAddress';
 import { signRaw } from './rpc';
 import { signJSON } from './rpc/signJSON';
@@ -7,26 +8,15 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
   origin,
   request,
 }) => {
-  let address;
-  await snap.request({
-    method: 'snap_manageState',
-    params: { operation: 'update', newState: { hello: 'world' } },
-  });
-
-  // At a later time, get the data stored.
-  const persistedData = await snap.request({
-    method: 'snap_manageState',
-    params: { operation: 'get' },
-  });
-
-  console.log('request.params:',request.params)
+  const _params = request.params;
   switch (request.method) {
     case 'signJSON':
-      return await signJSON(request.params.payload);
+      return _params?.payload && (await signJSON(_params?.payload));
     case 'signRaw':
-      return await signRaw(request.params.raw);
+      return _params?.raw && (await signRaw(request.params.raw));
     case 'getAddress':
-      return await createAddress(request.params.chainName);
+      return await createAddress(_params.chainName);
+
     default:
       throw new Error('Method not found.');
   }
