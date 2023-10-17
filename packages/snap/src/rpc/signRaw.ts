46dc21e7d7bb61c247d18c95bd9007e09dbfb345
Kami <ekbatanifard@gmail.com>
2023-10-17 17:53:49 +0300

    add signRaw

--- packages/snap/src/rpc/signRaw.ts
+++ packages/snap/src/rpc/signRaw.ts
@@ -1,23 +1,48 @@
+/* eslint-disable jsdoc/require-jsdoc */
 import { copyable, divider, heading, panel, text } from '@metamask/snaps-ui';
-import { SignerPayloadRaw } from '@polkadot/types/types';
+import { SignerPayloadRaw, SignerResult } from '@polkadot/types/types';
+import { isAscii, u8aToString, u8aUnwrapBytes } from '@polkadot/util';
+import { getKeyPair } from '../util/getKeyPair';
 
-const showConfirm = (data: string) => {
+const confirmation = (hexString: string) => {
+  const data = isAscii(hexString)
+    ? u8aToString(u8aUnwrapBytes(hexString))
+    : hexString;
+
   return panel([
     heading('A signature request is received'),
     divider(),
-    panel([copyable(data), text(data), divider()]),
+    text('Data to sign:'),
+    divider(),
+    panel([copyable(data), divider()]),
   ]);
 };
 
-export const signRaw = async (raw: SignerPayloadRaw): Promise<string> => {
+async function showConfirmSignRaw(
+  data: string,
+): Promise<string | boolean | null> {
+  const userResponse = await snap.request({
+    method: 'snap_dialog',
+    params: {
+      content: confirmation(data),
+      type: 'confirmation',
+    },
+  });
+
+  return userResponse;
+}
+
+export const signRaw = async (raw: SignerPayloadRaw): Promise<SignerResult> => {
   const { address, data } = raw;
-  const isConfirmed = showConfirm(data);
+  const isConfirmed = await showConfirmSignRaw(data);
 
   if (!isConfirmed) {
-    throw new Error('User rejects the signing!');
+    throw new Error('User declined the signing request.');
   }
+  const keypair = await getKeyPair();
 
-  // TODO
+  const signature = keypair.sign(data);
+  const hexSignature = Buffer.from(signature).toString('hex');
 
-  return 'signature';
+  return { id: 1, signature: `0x${hexSignature}` };
 };
