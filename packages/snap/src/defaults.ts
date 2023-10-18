7b3fae63d6f11bd49c237e79b0cd0452c464e232
Kami <ekbatanifard@gmail.com>
2023-10-18 21:44:55 +0300

    change default address to substrate

--- packages/snap/src/defaults.ts
+++ packages/snap/src/defaults.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_NETWORK_PREFIX = 42; // 42 is for substrate
 export const DEFAULT_COIN_TYPE = 354; // 354 is for Polkadot,
-export const DEFAULT_CHAIN_NAME = 'polkadot';
+export const DEFAULT_CHAIN_NAME = 'westend';
