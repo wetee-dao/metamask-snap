fcd8efb8f58de3c18784c0b1568c57fc104f606d
Kami <ekbatanifard@gmail.com>
2023-12-15 22:49:08 +0330

    Update getBalance.ts

--- packages/snap/src/util/getBalance.ts
+++ packages/snap/src/util/getBalance.ts
@@ -3,6 +3,10 @@ import type { AccountData } from '@polkadot/types/interfaces/balances/types';
 import { getApi } from './getApi';
 import { getFormatted } from './getFormatted';
 
+export type Balances = {
+  total: Balance;
+  transferable: Balance;
+};
 /**
  * To get the balance of an address
  *
@@ -12,17 +16,17 @@ import { getFormatted } from './getFormatted';
 export async function getBalances(
   genesisHash: string,
   address: string,
-): Promise<Balance> {
+): Promise<Balances> {
   const api = await getApi(genesisHash);
   const formatted = getFormatted(genesisHash, address);
 
   const balances = (await api.query.system.account(formatted)) as unknown as {
     data: AccountData;
   };
-  console.log('balances:', balances);
+  console.log('balances:', balances.data);
 
-  return api.createType(
-    'Balance',
-    balances.data.free.sub(balances.data.frozen),
-  ) as Balance;
+  const transferable = api.createType('Balance', balances.data.free.sub(balances.data.frozen),) as Balance;
+  const total = api.createType('Balance', balances.data.free.add(balances.data.reserved),) as Balance;
+
+  return { total, transferable };
 }
