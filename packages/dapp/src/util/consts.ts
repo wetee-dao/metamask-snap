aec77370735c6b56dca31585edb8e8d126ecbfe8
Kami <ekbatanifard@gmail.com>
2024-01-16 23:20:22 +0330

    change env var name

--- packages/dapp/src/util/consts.ts
+++ packages/dapp/src/util/consts.ts
@@ -1,9 +1,10 @@
-const SNAP_VERSION = '>=0.1.13';
+const SNAP_VERSION = '>=0.1.14';
 const LOCAL_SNAP_ID = 'local:http://localhost:8080';
 const NPM_SNAP_ID = 'npm:@polkagate/snap';
 
-export const DEFAULT_SNAP_ORIGIN = process.env.REACT_APP_MODE === 'development' ? LOCAL_SNAP_ID : NPM_SNAP_ID;
+export const DEFAULT_SNAP_ORIGIN = process.env.NODE_ENV === 'development' ? LOCAL_SNAP_ID : NPM_SNAP_ID;
 
+console.log('NODE_ENV:',process.env.NODE_ENV)
 export const SUPPORTED_SNAPS = {
   [DEFAULT_SNAP_ORIGIN]: { version: SNAP_VERSION },
 };
